292ca8f023754f72e342ff7795ac40c42e868fa7
kdjoaolol <77984397+kdjoaolol@users.noreply.github.com>
2022-03-16 16:04:25 -0300

    Excluir itens

--- filmes/tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .models import Filme
-
-def index(request):
-    filme = Filme.objects.all()
-    print(type(filme))
-    return render(request, 'filmes/index.html', {'filme': filme})
